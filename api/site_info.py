606da799251c5e83e1bccd9c45d9e8d02e947aca
Flavia Missi <flaviamissi@gmail.com>
2020-03-02 09:30:02 +0100

    update data

--- api/site_info.py
+++ api/site_info.py
@@ -4,4 +4,15 @@ site = {
     'skills': 'Software Developer in Malmö, Sweden',
     'keywords': 'Software Engineer, DevOps, Golang, Python, kubernetes, k8s, docker',
     'baseurl': '',
+    'social': [
+    {
+        'url': 'https://www.linkedin.com/in/flaviamissi/',
+        'title': 'linkedin'
+    },
+    {
+        'url': 'https://github.com/flaviamissi',
+        'title': 'github'
+    }
+
+    ]
 }
--- static/js/projects.js
+++ static/js/projects.js
@@ -12,10 +12,4 @@ $(function() {
             $( "#interests .other" ).append("<li>" + item + "</li>");
         });
     });
-
-    $.get( "/api/interests", function( data ) {
-        data.forEach( function( item, index ) {
-            $( "#interests .personal-interests" ).append("<li><b>" + item.title + "</b>" + item.description + "</li>");
-        });
-    });
 });
